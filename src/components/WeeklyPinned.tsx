b3cbb7c685e0720be12952f2bbe44d3cda364f9a
chenhaojie9527 <a18520145493@163.com>
2023-10-15 23:12:14 +0800

    feat: weekly pinned header

---
+++ src/components/WeeklyPinned.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WeeklyPinned: React.FC = () => {
+  return (
+    <div>
+      <header className="flex items-center justify-between">
+        <h1 className="text-[#2C2543] text-[24px] font-normal font-[Gilroy-Regular]">
+          Weekly Pinned
+        </h1>
+        <span className="text-[#F4B860] text[14px] font-bold font-[Gilroy-Bold]">
+          View all
+        </span>
+      </header>
+    </div>
+  );
+};
+
+export default WeeklyPinned;
--- src/layout/Sider.tsx
+++ src/layout/Sider.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Logo from "../components/Logo";
+import WeeklyPinned from "../components/WeeklyPinned";
 
 const Sider: React.FC = () => {
   return (
-    <div className="p-[60px]">
-      <Logo />
-    </div>
+    <ul className="p-[60px] w-full h-full">
+      <li className="mb-[76px]">
+        <Logo />
+      </li>
+      <li>
+        <WeeklyPinned />
+      </li>
+    </ul>
   );
 };
 
